72121ccb43f9423700f4c960a427c91aac4347ce
Martha Troubh <indexer@icloud.com>
2019-07-29 23:13:00 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190729180226) do
+ActiveRecord::Schema.define(version: 20190729195109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,13 +29,15 @@ ActiveRecord::Schema.define(version: 20190729180226) do
   end
 
   create_table "locales", force: :cascade do |t|
-    t.string "nickname", default: 'Primary'
+    t.string "nickname"
     t.string "address"
     t.string "city"
     t.string "state"
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_locales_on_user_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -95,6 +97,7 @@ ActiveRecord::Schema.define(version: 20190729180226) do
   end
 
   add_foreign_key "items", "merchants"
+  add_foreign_key "locales", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
